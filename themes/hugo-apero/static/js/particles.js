10eeac456c4bdfcf8fb09680742a89f5ec65382a
Marco Campanário <macscampanario@gmail.com>
2023-04-17 08:18:23 -0300

    c

--- themes/hugo-apero/static/js/particles.js
+++ themes/hugo-apero/static/js/particles.js
@@ -46,7 +46,7 @@ particlesJS("particles-js", {
     modes: {
       grab: { distance: 400, line_linked: { opacity: 1 } },
       bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
-      repulse: { distance: 100, duration: 0.3 },
+      repulse: { distance: 150, duration: 0.3 },
       push: { particles_nb: 4 },
       remove: { particles_nb: 2 }
     }
