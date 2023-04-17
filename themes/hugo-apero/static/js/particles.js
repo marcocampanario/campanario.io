4daff934ed2bc0701ba1d360c81c63034332fb67
Marco Campanário <macscampanario@gmail.com>
2023-04-17 08:41:24 -0300

    c

--- themes/hugo-apero/static/js/particles.js
+++ themes/hugo-apero/static/js/particles.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
   particles: {
     number: { value: 100, density: { enable: true, value_area: 800 } },
-    color: { value: "#EF86A6" },
+    color: { value: "#1F7F95" },
     shape: {
       type: "polygon",
       stroke: { width: 0, color: "#000000" },
